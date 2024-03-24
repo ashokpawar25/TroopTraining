fb250553b7d6200023ca70ebb545af7f3a16b6f4
ashokpawar25 <ashokpawar25052001@gmail.com>
2024-03-24 20:48:57 +0530

    Added test case to get count of Archer and Barbarian

--- TroopTraining/src/main/java/com/amaap/trooptraining/domain/ArmyCamp.java
+++ TroopTraining/src/main/java/com/amaap/trooptraining/domain/ArmyCamp.java
@@ -1,4 +1,7 @@
 package com.amaap.trooptraining.domain;
+import com.amaap.trooptraining.domain.model.Archer;
+import com.amaap.trooptraining.domain.model.Barbarian;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,5 +22,32 @@ public class ArmyCamp
     {
         trainedTroopers.add(trooper);
         return true;
+    }
+
+    public int getArcherCount()
+    {
+        int ArcherCount = 0;
+        for(Trooper trooper:trainedTroopers)
+        {
+            if(trooper instanceof Archer)
+            {
+                ArcherCount++;
+            }
+        }
+        return ArcherCount;
+    }
+
+
+    public int getBarbarianCount()
+    {
+        int BarbarianCount = 0;
+        for(Trooper trooper:trainedTroopers)
+        {
+            if(trooper instanceof Barbarian)
+            {
+                BarbarianCount++;
+            }
+        }
+        return BarbarianCount;
     }
 }
--- TroopTraining/src/test/java/com/amaap/trooptraining/domain/ArmyCampTest.java
+++ TroopTraining/src/test/java/com/amaap/trooptraining/domain/ArmyCampTest.java
@@ -2,6 +2,7 @@ package com.amaap.trooptraining.domain;
 
 import com.amaap.trooptraining.domain.exceptions.InvalidTrainingCostException;
 import com.amaap.trooptraining.domain.exceptions.InvalideTrainingTimeException;
+import com.amaap.trooptraining.domain.model.Archer;
 import com.amaap.trooptraining.domain.model.Barbarian;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,28 @@ public class ArmyCampTest {
         Assertions.assertEquals(trooper, troopers.get(0));
     }
 
-    
+    @Test
+    void shouldAbleToGetCountOfArcherAndBarbarian() throws InvalideTrainingTimeException, InvalidTrainingCostException {
+        //Arrange
+        ArmyCamp armyCamp = new ArmyCamp();
+        Trooper Archer1 = new Archer();
+        Trooper Archer2 = new Archer();
+        Trooper Barbarian1 = new Barbarian();
+        Trooper Barbarian2 = new Barbarian();
+
+        //Act
+        armyCamp.addTrooper(Archer1);
+        armyCamp.addTrooper(Archer2);
+        armyCamp.addTrooper(Barbarian1);
+        armyCamp.addTrooper(Barbarian2);
+
+        int expectedArcherCount = armyCamp.getArcherCount();
+        int expectedBarbarianCount = armyCamp.getBarbarianCount();
+
+        //Assert
+        Assertions.assertEquals(2, expectedArcherCount);
+        Assertions.assertEquals(2, expectedBarbarianCount);
+
+    }
 
 }
